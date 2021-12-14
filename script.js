b2b693974a0a0f1cc0b343d5985d9b0b6f19962a
Reem <janinareem@gmail.com>
2021-12-14 17:33:28 +0200

    Update script.js file

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-function mobileMenu () {
+function mobileMenu() {
   const menuIcon = document.querySelector('#menu-icon');
   const mainNav = document.querySelector('.main-nav');
   const exitIcon = document.querySelector('.exit');
   const navLinks = document.querySelectorAll('.ul-menu li');
   const body = document.querySelector('body');
-  const intro = document.querySelector('#intro');
 
   menuIcon.addEventListener('click', () => {
     mainNav.classList.add('main-nav-active');
-    intro.classList.add('blurry');
     body.classList.add('overflow-hidden');
   });
   exitIcon.addEventListener('click', () => {
     mainNav.classList.remove('main-nav-active');
-    intro.classList.remove('blurry');
     body.classList.remove('overflow-hidden');
   });
   navLinks.forEach((link) => {
     link.addEventListener('click', () => {
-      intro.classList.remove('blurry');
       mainNav.classList.remove('main-nav-active');
       body.classList.remove('overflow-hidden');
     });
